a714ca944f48bb40df14482d2ad5712da3fe0fd2
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-02 12:29:35 +0200

    :lipstick: Add pomodoro design

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import PomodoroTimer from './components/PomodoroTimer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>New Project</h1>
+    <PomodoroTimer />
   </React.StrictMode>
 );
